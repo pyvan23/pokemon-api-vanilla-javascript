4504de73e8d36973224a068dd988697af5c3eb7e
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-01-30 13:14:29 -0300

    pokemon api card terminada

--- js/app.js
+++ js/app.js
@@ -15,7 +15,11 @@ const FetchApi = async (id) => {
     const pokemon = {
       img: data.sprites.other.dream_world.front_default,
       name: data.name,
-      hp:data.stats[0].base_stat,
+      hp: data.stats[0].base_stat,
+      experience: data.base_experience,
+      atack: data.stats[1].base_stat,
+      special:data.stats[3].base_stat,
+      defense:data.stats[2].base_stat,
     };
     paintData(pokemon);
   } catch (error) {
@@ -26,6 +30,7 @@ const paintData = (pokemon) => {
   console.log(pokemon);
   const flex = document.querySelector(".flex");
   //content por que necesito lo que esta dento de las etiquetas template no en esa
+  //no modificamos el dom sino el clon
   const template = document.querySelector("#template-api").content;
   const clone = template.cloneNode(true);
   const fragment = document.createDocumentFragment();
@@ -36,6 +41,17 @@ const paintData = (pokemon) => {
   //innerHTML usamos por que nos permite mezclar las etiquetas
   clone.querySelector(".card-body-title").innerHTML = ` ${pokemon.name}
   <span>Hp: ${pokemon.hp}</span>`;
+
+  clone.querySelector(".card-body-text").textContent =
+    pokemon.experience + " Exp";
+  //querySelectorAll por que comparten la misma clase los 3,este metodo nnos decuelve un array
+  clone.querySelectorAll(".card-footer-social h3")[0].textContent =
+    pokemon.atack + 'K';
+  clone.querySelectorAll(".card-footer-social h3")[1].textContent =
+    pokemon.special + 'K';
+  clone.querySelectorAll(".card-footer-social h3")[2].textContent =
+    pokemon.defense + 'K';
+
   fragment.appendChild(clone);
   flex.appendChild(fragment);
 };
